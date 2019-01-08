f267f28028f8501643396b606358b8181c133667
1711605020 <651525795@qq.com>
2019-01-08 16:33:16 +0800

    代码上传2

--- app/src/main/java/com/example/classtimetable/AddCourseActivity.java
+++ app/src/main/java/com/example/classtimetable/AddCourseActivity.java
@@ -14,6 +14,41 @@ public class AddCourseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_course);
+        setFinishOnTouchOutside(false);
 
+        final EditText inputCourseName = (EditText) findViewById(R.id.course_name);
+        final EditText inputTeacher = (EditText) findViewById(R.id.teacher_name);
+        final EditText inputClassRoom = (EditText) findViewById(R.id.class_room);
+        final EditText inputDay = (EditText) findViewById(R.id.course_week);
+        final EditText inputStart = (EditText) findViewById(R.id.class_begin);
+        final EditText inputEnd = (EditText) findViewById(R.id.class_ends);
+
+        Button okButton=(Button)findViewById(R.id.button);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String courseName = inputCourseName.getText().toString();
+                String teacher = inputTeacher.getText().toString();
+                String classRoom = inputClassRoom.getText().toString();
+                String day = inputDay.getText().toString();
+                String start = inputStart.getText().toString();
+                String end = inputEnd.getText().toString();
+
+                if (courseName.equals("") || day.equals("") || start.equals("") || end.equals("")) {
+                    Toast.makeText(AddCourseActivity.this, "基本课程信息未填写完成", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Course course = new Course(courseName, teacher, classRoom,
+                            Integer.valueOf(day), Integer.valueOf(start), Integer.valueOf(end));
+                    Intent intent = new Intent(AddCourseActivity.this, MainActivity.class);
+                    intent.putExtra("course", course);
+
+                    setResult(0, intent);
+                    finish();
+                }
+            }
+        });
     }
 }
+
+
